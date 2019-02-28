34748c4bcbdf23916d2d1092884f38560c73c0b3
Thibault <thibaultgiordan@gmail.com>
2019-02-28 19:33:36 +0100

    kebab pizza

--- script/script_thibault.py
+++ script/script_thibault.py
@@ -1,26 +1,24 @@
 def parsing(file_name) :
     path_in = "../../data/in/"
 
-    with open(path_in + file_name) as f:
+    with open(path_in + "a_example.txt") as f:
         for i, line in enumerate(f):
             element = line.replace("\n", "").strip()
-            dict_line = dict()
+
             if i == 0:
                 num_line = int(element)
-                tab_global = []
+                dict_line = {}
             else:
                 tab_line = element.split(" ")
                 h_or_v = tab_line[0]
                 num_tag = tab_line[1]
                 list_tag = tab_line[2:]
-                # dict_line={}
-                dict_line["id"] = [i - 1]
-                dict_line["h_or_v"] = h_or_v
-                dict_line["num_tag"] = num_tag
-                dict_line["list_tag"] = sorted(list_tag)
-                tab_global.append(dict_line)
+                dict_line[i - 1] = {}
+                dict_line[i - 1]["h_or_v"] = h_or_v
+                dict_line[i - 1]["num_tag"] = num_tag
+                dict_line[i - 1]["list_tag"] = sorted(list_tag)
 
-    data = (num_line, tab_global)
+    data = (num_line, dict_line)
     return data
 
 def found_max_tag(data) :
